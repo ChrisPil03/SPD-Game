81eb624d19ff41742b4da395150ecfd76d0f0ff1
ChrisPil03 <chris@pilegard.se>
2024-02-06 19:49:40 +0100

    small bugfixes
    no longer getting stuck on walls

--- Assets/Scripts/SkillTree.cs
+++ Assets/Scripts/SkillTree.cs
@@ -13,7 +13,7 @@ public class SkillTree : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && skillTree != null)
         {
             interactPopUp.SetActive(true);
             canAccessSkillTree = true;
