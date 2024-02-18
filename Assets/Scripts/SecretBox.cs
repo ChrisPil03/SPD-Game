d935e20fc93de796ec148585a340e2eabdd6ed8e
ChrisPil03 <chris@pilegard.se>
2024-02-18 19:28:53 +0100

    Background

--- Assets/Scripts/SecretBox.cs
+++ Assets/Scripts/SecretBox.cs
@@ -1,16 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SecretBox : MonoBehaviour
 {
     [SerializeField] private GameObject interactionIcon;
 
+    private bool canOpenBox;
+    private bool hasBeenOpened;
+
+    [SerializeField] private SpriteRenderer[] childRenderers;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             interactionIcon.SetActive(true);
+            canOpenBox = true;
         }
     }
 
@@ -19,6 +23,36 @@ public class SecretBox : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             interactionIcon.SetActive(false);
+            canOpenBox = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canOpenBox)
+        {
+            interactionIcon.SetActive(false);
+            hasBeenOpened = true;
+            Destroy(gameObject, 2f);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (hasBeenOpened)
+        {
+            FadeAway();
+        }
+    }
+
+    private void FadeAway()
+    {
+        foreach (SpriteRenderer r in childRenderers)
+        {
+            Color alphaColor = r.material.color;
+            alphaColor.a = 0f;
+
+            r.material.color = Color.Lerp(r.material.color, alphaColor, 2 * Time.deltaTime);
         }
     }
 }
